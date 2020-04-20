3d84878792860297042d2bed6655b0147a9e7e56
awelch17 <awelch17@geffenacademy.ucla.edu>
2020-04-20 14:31:32 -0700

    fixed cookie expiration

--- george-checklist/cookies.js
+++ george-checklist/cookies.js
@@ -9,7 +9,10 @@ function convert(state) {
 
 
 function setCookie(cname, cvalue) {
-    document.cookie = encodeURIComponent(cname) + '=' + encodeURIComponent(cvalue);
+  var d = new Date();
+  d.setTime(d.getTime() + (365*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
   
 
